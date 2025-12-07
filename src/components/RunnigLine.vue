fb1b7c81f4afb27bc9b0f8ed97aa28cfee153178
maria <m.guselnikova667@gmail.com>
2025-12-07 20:35:58 +0400

    add run

--- src/components/RunnigLine.vue
+++ src/components/RunnigLine.vue
@@ -1,39 +1,62 @@
 <template>
-  <div class="marquee-wrapper">
-    <div class="marquee">
-      • Присоединяйся • Присоединяйся • Присоединяйся • Присоединяйся • Присоединяйся • Присоединяйся • Присоединяйся • Присоединяйся • Присоединяйся •
+  <div class="marquee">
+  <div class="marquee-track">
+    <div class="marquee-content">
+      <span>• Присоединяйся •&nbsp;</span>
+      <span>• Присоединяйся •&nbsp;</span>
+      <span>• Присоединяйся •&nbsp;</span>
+      <span>• Присоединяйся •&nbsp;</span>
     </div>
+    <div class="marquee-content">
+      <span> Присоединяйся •&nbsp;</span>
+      <span>• Присоединяйся •&nbsp;</span>
+      <span>• Присоединяйся •&nbsp;</span>
+      <span>• Присоединяйся •&nbsp;</span>
+    </div>
+     <div class="marquee-content">
+      <span> Присоединяйся •&nbsp;</span>
+      <span>• Присоединяйся •&nbsp;</span>
+      <span>• Присоединяйся •&nbsp;</span>
+      <span>• Присоединяйся •&nbsp;</span>
+    </div>
+  </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'MarqueeText'
-}
-</script>
-
-<style scoped>
-.marquee-wrapper {
-  width: 100%;
-  overflow: hidden;
-  background-color: #7B4BFF; /* фиолетовый фон */
-}
-
+<style>
 .marquee {
-  display: inline-block;
-  white-space: nowrap;
-  padding: 10px 0;
-  color: white;
-  font-weight: bold;
-  animation: marquee 10s linear infinite;
+  width: 100%;     
+  height: 100px;  
+  background-color: #6f239c;
+  display: flex;
+  align-items: center;
+  overflow: hidden; 
+  position: relative;
 }
 
-@keyframes marquee {
-  0% {
-    transform: translateX(100%);
-  }
-  100% {
-    transform: translateX(-100%);
-  }
+.marquee-track {
+  display: flex;
+  animation: scroll-left 20s linear infinite;
+  white-space: nowrap;
+ 
+  min-width: 200%; 
+}
+
+.marquee-content {
+  display: flex;
+  flex-shrink: 0; 
+  flex: 0 0 50%; 
+}
+
+ .marquee-content span {
+  white-space: nowrap;
+  font-size: 20px;
+  color: white;
+  margin-right: 10px;
+}
+
+@keyframes scroll-left {
+  from { transform: translateX(0); }
+  to   { transform: translateX(-50%); }
 }
 </style>
