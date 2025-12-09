5df177d2e8371f019f0a6ebb391e303aba46f3e5
katya <katyakochetkova11@gmail.com>
2025-12-09 15:12:01 +0400

    add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,53 @@
 <template>
-    <div>
-        <h2>Footer</h2>
-    </div>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer-wrapper">
+                <div class="left-footer">
+                    <h3 class="footer-title" >Контакты</h3>
+                    <div class="contacts">
+                    <a class="footer-phone" href="tel:+7 927 268-50-15">+7 927 268-50-15</a>
+                    <a href="malito:vkonst@inbox.ru" class="footer-phone">vkonst@inbox.ru</a>
+                    </div>
+                    <div class="adress">
+                        <h3 class="footer-title" >Адрес</h3>
+                        <div class="footer-adress">
+                            Белорусская улица, 6, Центральный район, Тольятти
+                        </div>
+                    </div>
+                </div>
+                <div class="right-wrapper">
+                    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A0b34477627d35662a4c390d61e0ceaefa8761e2219fb7cb0b2bcd0599a3f7197&amp;source=constructor" width="591" height="302" frameborder="0"></iframe>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
+
+<style>
+
+.footer {
+    background-color:#6f239c;
+}
+.footer-wrapper {
+    display: flex;
+    justify-content: space-between;
+    padding: 20px 0;
+}
+.footer-title {
+    color: white;
+    font-size: 28px;
+}
+.footer-phone {
+    display: block;
+    font-size: 20px;
+    color: white;
+    text-decoration: none;
+    line-height: 190%;
+}
+.footer-adress {
+    color: white;
+    font-size: 20px;
+}
+
+
+</style>
